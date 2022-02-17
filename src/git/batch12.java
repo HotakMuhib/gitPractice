6318fedbb2289a00ed782b2d7d2666762f862ed2
HotakMuhib <123457don@gmail.com>
2022-02-16 21:56:31 -0500

    This is the 3rd commit

--- src/git/batch12.java
+++ src/git/batch12.java
@@ -5,7 +5,8 @@ public class batch12 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("we are we are");
-		System.out.println("We     are ");
+		System.out.println("We     are");
+		System.out.println("This is the 3rd commit");
 
 	}
 
